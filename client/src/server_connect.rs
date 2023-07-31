ee0ba34b9fceca83558f841f66cf0bd34743567b
Chase Sunstrom <122314695+ChaseSunstrom@users.noreply.github.com>
2023-07-31 13:19:43 -0500

    Added a permanent connection.

--- client/src/server_connect.rs
+++ client/src/server_connect.rs
@@ -4,8 +4,8 @@ use std::str::from_utf8;
 use crate::input::get_input;
 
 
-pub fn connect(ip: String, port: u16) {
-    let split_ip = ip.split('.')
+pub fn connect(/*ip: String, port: u16*/) {
+    /*let split_ip = ip.split('.')
         .map(|x| x.parse::<u8>().unwrap())
         .collect::<Vec<u8>>();
 
@@ -19,47 +19,62 @@ pub fn connect(ip: String, port: u16) {
             ),
             port
         )
+    );*/
+
+    let socket = SocketAddr::V4(
+        SocketAddrV4::new(
+            Ipv4Addr::new(
+                192,
+                168,
+                1,
+                151,
+            ),
+            8080
+        )
     );
 
-    let mut msg = get_input();
 
-    const BUFFER_SIZE: usize = 100;
+    loop {
+        let mut msg = get_input();
 
-    if msg.len() < BUFFER_SIZE {
-        while msg.len() < BUFFER_SIZE {
-            msg.push_str(" ");
-        }
-    } else if msg.len() > BUFFER_SIZE{
-         msg.truncate(BUFFER_SIZE);
-    }
+        const BUFFER_SIZE: usize = 100;
 
-    match TcpStream::connect(socket) {
-        Ok(mut stream) => {
-            //bincode::serialize(&message).unwrap();
-
-            stream.write(msg.as_bytes()).unwrap();
-
-            let mut buffer = [0 as u8; BUFFER_SIZE];
-
-            match stream.read_exact(&mut buffer) {
-                Ok(_) => {
-                    if &buffer == msg.as_bytes() {
-                        println!("[SERVER] Message sent: {}", msg);
-                    } else {
-                        let text = from_utf8(&buffer).unwrap();
-                        println!("[SERVER] Failed to recieve message: {}\n
-                                           Got instead: {}", msg, text);
-                    }
-                },
-                Err(e) => {
-                    println!("[SERVER] Failed to recieve message: {}", e);
-                }
+        if msg.len() < BUFFER_SIZE {
+            while msg.len() < BUFFER_SIZE {
+                msg.push_str(" ");
             }
-        },
-        Err(e) => {
-            println!("[SERVER] Failed to connect: {}", e);
+        } else if msg.len() > BUFFER_SIZE{
+            msg.truncate(BUFFER_SIZE);
         }
-    }
 
-    println!("Connection closed.");
+        match TcpStream::connect(socket) {
+            Ok(mut stream) => {
+                //bincode::serialize(&message).unwrap();
+
+                stream.write(msg.as_bytes()).unwrap();
+
+                let mut buffer = [0 as u8; BUFFER_SIZE];
+
+                match stream.read_exact(&mut buffer) {
+                    Ok(_) => {
+                        if &buffer == msg.as_bytes() {
+                            println!("[SERVER] Message sent: {}", msg);
+                        } else {
+                            let text = from_utf8(&buffer).unwrap();
+                            println!("[SERVER] Failed to recieve message: {}\n
+                                           Got instead: {}", msg, text);
+                        }
+                    },
+                    Err(e) => {
+                        println!("[SERVER] Failed to recieve message: {}", e);
+                    }
+                }
+            },
+            Err(e) => {
+                println!("[SERVER] Failed to connect: {}", e);
+            }
+        }
+
+        println!("Connection closed.");
+    }
 }
