525f5684116e70c13d460c65ddffae7b5b0a08c8
Chase Sunstrom <Casunstrom+github@gmail.com>
2023-08-25 14:35:32 -0500

    Allows user to enter in IP and Port.

--- client/src/main.rs
+++ client/src/main.rs
@@ -10,9 +10,18 @@ mod message;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let (ip, port) = parse_input(
-        Some(args[1].parse::<String>().unwrap()),
-        Some(args[2].parse::<u16>().unwrap()));
+    let (ip, port) = match args.len() {
+        3 => {
+            parse_input(
+                Some(args[1].parse::<String>().unwrap()),
+                Some(args[2].parse::<u16>().unwrap())
+            )
+        }
+
+        _ => {
+            parse_input(None, None)
+        }
+    };
 
     connect(ip, port);
 }
